b52f4c056634e22936084063a9315e8cf137f28b
yangahh <toaur6802@naver.com>
2024-04-11 22:32:50 +0900

    Create project

---
+++ src/main/java/org/example/springsecurityoauthjwt/SpringSecurityOauthJwtApplication.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityoauthjwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityOauthJwtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityOauthJwtApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springsecurityoauthjwt/SpringSecurityOauthJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springsecurityoauthjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityOauthJwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
