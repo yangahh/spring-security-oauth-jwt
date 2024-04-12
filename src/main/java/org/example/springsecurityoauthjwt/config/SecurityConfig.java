785fcb00e02093c503f3042889b0a6b01bf52eec
yangahh <toaur6802@naver.com>
2024-04-12 15:35:57 +0900

    Add: naver oauth setting and db setting

--- src/main/java/org/example/springsecurityoauthjwt/config/SecurityConfig.java
+++ src/main/java/org/example/springsecurityoauthjwt/config/SecurityConfig.java
@@ -2,8 +2,10 @@ package org.example.springsecurityoauthjwt.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
